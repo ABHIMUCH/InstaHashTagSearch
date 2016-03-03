295162141ab954f7657bb38427cac93c71f61b39
ABHIMUCH <abhi.much@gmail.com>
2016-03-02 19:39:42 -0500

    initial code

---
+++ feed.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+    var feed = new Instafeed({
+        get: 'tagged',
+        tagName: 'awesome',
+        clientId: 'ce9355ec67544c458c17eb8cc68e1a26'
+    });
+    feed.run();
+</script>
